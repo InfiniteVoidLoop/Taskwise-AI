42ed0f19ef05894a8f2736486579670b65acb58b
unknown <jackdang2005@gmail.com>
2025-09-10 01:49:22 +0700

    simple firebase + adding redux

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/RightPanel.css'
+import {addNote, deleteNote} from '../models/firebase'
+
 function RightPanel(){
     return(
         <div className = "right-panel-container">
@@ -7,8 +9,10 @@ function RightPanel(){
                 <div className = 'right-panel-title'>
                     Your list
                 </div>
-                <button className = 'right-panel-add-button'>+</button>
-            </div>
+                <button className = 'right-panel-add-button'
+                        onClick={() => deleteNote('phuc', 323131)}
+                >+</button>
+            </div>  
             <div className = "right-panel-list">
 
             </div>
---
+++ src/models/firebase.ts
@@ -0,0 +1,67 @@
+import { initializeApp } from "firebase/app";
+import {getDatabase, set, ref, push, query, remove, equalTo, orderByChild, get} from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+async function addUser(username: string, password: string): Promise<boolean>{
+  try{
+    const userRef = ref(database, 'members');
+    const newUserRef = push(userRef);
+    await set(newUserRef, {
+      username: username, 
+      password: password
+    });
+    console.log('Successfuly add user');
+    return true;
+  }catch(error){
+    console.error(error);
+    return false;
+  }
+}
+
+async function addNote(username: string, title: string, description: string, timestamp: number): Promise<boolean>{
+  try{
+    const noteRef = ref(database, username);
+    const newNoteRef = push(noteRef);
+    await set(newNoteRef, {
+      title: title, 
+      description: description,
+      timestamp: timestamp
+    });
+    return true;
+  }catch(error){
+    console.error(error);
+    return false;
+  }
+}
+
+async function deleteNote(username: string, timestamp: number){
+  try{
+    const noteRefCollection = ref(database, username);
+    const noteRef = query(noteRefCollection, orderByChild('timestamp'), equalTo(timestamp));
+    const snapshot = await get(noteRef);
+    if (snapshot.exists()){
+      snapshot.forEach((childSnap) => {
+        remove(ref(database, `${username}/${childSnap.  key}`))
+      })
+    }
+    return true;
+  }catch(error){
+    console.error(error);
+    return false;
+  }
+}
+
+export {addUser, addNote, deleteNote};  
